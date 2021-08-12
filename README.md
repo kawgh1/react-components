010fc7ebf938e7ea3a6441d8d495855b5b89f20c
thrwwy216 <thrwwy216@tutanota.com>
2021-08-11 22:45:46 -0500

    update

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Create React App - npm start
 
 - ### Tools used 
 - #### Semantic UI CDN
+- https://semantic-ui.com/views/card.html
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail'
+import faker from 'faker'
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                    <h3>Warning!</h3>
+                    Are you sure you want to do that?
+                </div>
+            </ApprovalCard>
 
-            <CommentDetail></CommentDetail>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45pm" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 2:45pm" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Jack" timeAgo="Today at 2:55am" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Rick James Bitch" />
+            </ApprovalCard>
+
+            <ApprovalCard>\
+                <CommentDetail author="what" />
+            </ApprovalCard>
+
+
+
+
         </div>
 
     );
